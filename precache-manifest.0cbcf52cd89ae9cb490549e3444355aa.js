d287ed506652e26822bcf604d5116bc5dab9ec34
darksbond <ab.mushfik@gmail.com>
2022-03-04 00:08:34 +0600

    Updates

---
+++ precache-manifest.0cbcf52cd89ae9cb490549e3444355aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "003508c0fbe9688e206d853c0305bcb4",
+    "url": "/imageGallery/index.html"
+  },
+  {
+    "revision": "4602bbe29accb90b540f",
+    "url": "/imageGallery/static/css/2.ef2319b8.chunk.css"
+  },
+  {
+    "revision": "e9899d43a4f6687c1dc2",
+    "url": "/imageGallery/static/css/main.2f8f931b.chunk.css"
+  },
+  {
+    "revision": "4602bbe29accb90b540f",
+    "url": "/imageGallery/static/js/2.f5cf3ba9.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/imageGallery/static/js/2.f5cf3ba9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9899d43a4f6687c1dc2",
+    "url": "/imageGallery/static/js/main.48254dcd.chunk.js"
+  },
+  {
+    "revision": "faa69c73f3d79624dede",
+    "url": "/imageGallery/static/js/runtime-main.c0df8cfc.js"
+  }
+]);
